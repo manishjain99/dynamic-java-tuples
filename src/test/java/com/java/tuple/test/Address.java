fa3e59869296645e325f7d73abfdb72fb50b44c5
Jain <mjain@LM-SJN-00637012.(none)>
2013-04-17 08:54:01 -0700

    adding test, plus every tuple had english name insteadof ABC..

---
+++ src/test/java/com/java/tuple/test/Address.java
@@ -0,0 +1,28 @@
+package com.java.tuple.test;
+
+import org.dynamicjavatuples.Tuple;
+import org.dynamicjavatuples.TupleFactory;
+import org.dynamicjavatuples.impl.TupleNumber;
+
+public interface Address<A,B,C,D>  extends Tuple{
+	@TupleNumber(id=0)
+	public A getStreetAddress();
+	
+	@TupleNumber(id=1)
+	public B getCity();
+	
+	@TupleNumber(id=2)
+	public C getZip();
+	
+	@TupleNumber(id=3)
+	public D country();
+	
+	public static class createInstance extends TupleFactory{
+		@SuppressWarnings("unchecked")
+		public static <A,B,C,D,E extends Address<A,B,C,D>> E with(A a, B b, C c, D d){
+			return (E) getInstance(Address.class, a,b,c,d);
+		}
+	}
+
+
+}
---
+++ src/test/java/com/java/tuple/test/BirthInfo.java
@@ -0,0 +1,28 @@
+package com.java.tuple.test;
+
+import java.util.Date;
+
+import org.dynamicjavatuples.Tuple;
+import org.dynamicjavatuples.TupleFactory;
+import org.dynamicjavatuples.impl.TupleNumber;
+
+public interface BirthInfo<A,B,C> extends Tuple {
+	@TupleNumber(id=0)
+	public String getName();
+	
+	@TupleNumber(id=1)
+	public Date getDate();
+	
+	@TupleNumber(id=2)
+	public Address getBirthPlace();
+	
+	public static class createInstance extends TupleFactory{
+		@SuppressWarnings("unchecked")
+		public static <A,B,C,D extends BirthInfo> D with(A a, B b, C c, D d){
+			return (D) getInstance(BirthInfo.class, a,b,c,d);
+		}
+	}
+
+
+
+}
